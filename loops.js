569cafa1288daf203caed79ab27d76b6108c7290
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-29 21:12:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
-  for (let j = 0; j <= 25; j++){
-    array.push("I am" + i + "strange loops.");
+  for (let i = 0; i <= 25; i++){
+    array.push("I am " + i + " strange loops.");
   }
   return array;
 }
